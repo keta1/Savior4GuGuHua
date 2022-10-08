c1a88bd8e31d75c600b4abf89b8b862c544371ee
keta1 <k@ketal.icu>
2022-10-09 07:14:18 +0800

    rename File

--- libs/dexkit/src/main/kotlin/icu/ketal/util/Log.kt
+++ libs/dexkit/src/main/kotlin/icu/ketal/util/Log.kt
@@ -4,7 +4,7 @@ import com.sun.jna.Callback
 import com.sun.jna.Library
 
 @Suppress("FunctionName", "ClassName")
-interface NativeLib : Library {
+interface Log : Library {
     fun interface log_callback_t : Callback {
         operator fun invoke(log: String)
     }
@@ -12,6 +12,6 @@ interface NativeLib : Library {
     fun setup_log_stream(verbose: Boolean, callback: log_callback_t)
 
     companion object {
-        val instance: NativeLib by nativeLib("log")
+        val instance: Log by nativeLib("log")
     }
 }
